ffbb64f753eaf7ea90a634d875ed4fc72d5ae8f5
Jason <pellaea@gmail.com>
2020-04-12 15:15:14 -0400

    Added manifest.js

---
+++ app/assets/config/manifest.js
@@ -0,0 +1,8 @@
+//= link_tree ../images
+//= link_directory ../javascripts .js
+//= link Admin.css
+//= link Agaricus.css
+//= link Amanita.css
+//= link BlackOnWhite.css
+//= link Cantharellaceae.css
+//= link Hygrocybe.css
